bd73f322db4ec54da785e410a7ae65c206084781
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-01-26 17:38:42 -0300

    implememnt findBookByTitleWithQueryNamed using @query in book repository

--- src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
+++ src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
@@ -3,6 +3,7 @@ package guru.springframework.jdbc.repositories;
 import guru.springframework.jdbc.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.Nullable;
 import org.springframework.scheduling.annotation.Async;
 
@@ -11,7 +12,8 @@ import java.util.concurrent.Future;
 import java.util.stream.Stream;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+    @Query("SELECT b FROM Book b WHERE b.title = :title")
+    Book findBookByTitleWithQueryNamed(@Param("title") String title);
     @Query("SELECT b FROM Book b WHERE b.title = ?1")
     Book findBookByTitleWithQuery(String title);//It's not using the naming convention
     Optional<Book> findBookByTitle(String title);
