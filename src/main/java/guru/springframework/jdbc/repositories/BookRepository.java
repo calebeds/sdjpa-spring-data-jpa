1d1382e564642e59c625a2f52cc15f7374c179cc
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-01-26 15:50:34 -0300

    add null handling

--- src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
+++ src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
@@ -2,9 +2,13 @@ package guru.springframework.jdbc.repositories;
 
 import guru.springframework.jdbc.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.Nullable;
 
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findBookByTitle(String title);
+    @Nullable
+    Object getByTitle(@Nullable String title);
+    Book readByTitle(String title);
 }
